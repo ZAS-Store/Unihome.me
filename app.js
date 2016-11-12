ed2aa1feb785d60e8c145600535dc649663ae975
Vincent Teng <vincentteng@Vincents-MacBook-Pro.local>
2016-11-12 01:45:27 -0500

    Port

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+var http = require('http');
+
+var dns = require('dns');
+var cors = require('cors');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.set('ip', process.env.IP);
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/Site'));
+
+var port = process.env.PORT || 8080;
+app.get('/', function(req, res) {
+  res.sendFile(__dirname+'/Site/index.html');
+});
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
